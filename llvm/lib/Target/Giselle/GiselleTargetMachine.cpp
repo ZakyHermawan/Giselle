f07d999b9ea10907a6a749f0dc3b9579c18a9718
ZakyHermawan <zaky.hermawan9615@gmail.com>
2026-04-05 14:22:06 +0700

    Build with new experimental target
    Signed-off-by: ZakyHermawan <zaky.hermawan9615@gmail.com>

---
+++ llvm/lib/Target/Giselle/GiselleTargetMachine.cpp
@@ -0,0 +1,15 @@
+//===-- GiselleTargetMachine.cpp - Define TargetMachine for Giselle -----------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+//
+// Implements the info about Giselle target spec.
+//
+//===----------------------------------------------------------------------===//
+
+#include "llvm/Support/Compiler.h" // For LLVM_EXTERNAL_VISIBILITY.
+
+extern "C" LLVM_EXTERNAL_VISIBILITY void LLVMInitializeGiselleTarget() {}
---
+++ llvm/lib/Target/Giselle/TargetInfo/GiselleTargetInfo.cpp
@@ -0,0 +1,11 @@
+//===-- GiselleTargetInfo.cpp - Giselle Target Implementation -----------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+#include "llvm/Support/Compiler.h" // For LLVM_EXTERNAL_VISIBILITY.
+
+extern "C" LLVM_EXTERNAL_VISIBILITY void LLVMInitializeGiselleTargetInfo() {}

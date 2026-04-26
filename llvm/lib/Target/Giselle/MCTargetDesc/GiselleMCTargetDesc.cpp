a2eb116a5b63f5fc97ba1be92c0bc0edf4314175
ZakyHermawan <zaky.hermawan9615@gmail.com>
2026-04-26 22:10:11 +0700

    Enable end to end testing (LLVMIR to RV32I assembly)
    Signed-off-by: ZakyHermawan <zaky.hermawan9615@gmail.com>

--- llvm/lib/Target/Giselle/MCTargetDesc/GiselleMCTargetDesc.cpp
+++ llvm/lib/Target/Giselle/MCTargetDesc/GiselleMCTargetDesc.cpp
@@ -26,6 +26,7 @@ using namespace llvm;
 
 #define GET_INSTRINFO_MC_DESC
 #define GET_INSTRINFO_MC_HELPERS
+#define ENABLE_INSTR_PREDICATE_VERIFIER
 #include "GiselleGenInstrInfo.inc"
 
 static MCSubtargetInfo *
